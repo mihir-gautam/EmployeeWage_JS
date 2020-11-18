413bc53095cbacce2f08194023da077687ceaef6
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-18 23:51:22 +0530

    [Mihir] Add. Ability to store daily wage in an array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,32 +1,42 @@
-//UC2 Calculate daily wage based on part time or full time
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-let empHrs =0;
-//UC3 function for daily working hours
-function getWorkingHours(empCheck){
-    switch(empCheck){
-        case IS_PART_TIME:
-            return PART_TIME_HOURS;
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-        default:
-            return 0;
-    }
+//UC 6 Store Daily Wage along with total wage 
+const MAX_HRS_IN_MONTH=160;
+const NUM_OF_WORKING_DAYS=20;
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const WAGE_PER_HOUR=20;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+let empDailyWageArr=new Array();
+ 
+function calculateDailyWage(empHrs)
+{
+  return empHrs*WAGE_PER_HOUR;
 }
+ 
+function getWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+       case IS_PART_TIME:
+            return 4;
+       case IS_FULL_TIME:
+            return 8;
+       default:
+            return 0;
+   }
+} 
+while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+{
+  totalWorkingDays++;
+  
+  let empCheck=Math.floor(Math.random()*10)%3;
+  let empHrs=getWorkingHours(empCheck);
+  totalEmpHrs+=empHrs;
+  empDailyWageArr.push(calculateDailyWage(empHrs));
+}
+ 
+let empWage=calculateDailyWage(totalEmpHrs);
+console.log(empDailyWageArr);
+console.log("Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
+ 
 
-//UC5 Calculate wages till 160 hours or 20 working days reached 
-const MAX_HRS_IN_MONTH = 160;
-const NUM_OF_WORKING_DAYS = 20;
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-while (totalEmpHrs <=MAX_HRS_IN_MONTH &&
-     totalWorkingDays < NUM_OF_WORKING_DAYS){
-         totalWorkingDays++;
-         let empCheck = Math.floor(Math.random() * 10) % 3;
-         totalEmpHrs+=getWorkingHours(empCheck);
-     }
-     let empWage = totalEmpHrs * WAGE_PER_HOUR;
-     console.log("Total Days: "+ totalWorkingDays+ " Total Hrs: "+totalEmpHrs+" Emp Wage: "+ empWage); 
