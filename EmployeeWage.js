fea2855c3faaf96d6347f2079a2149c14945dcb6
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-18 23:10:11 +0530

    [Mihir] Add. Ability to check employee is present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,41 +1,10 @@
-//UC 6 Store daily wage along with total wage
-const MAX_HRS_IN_MONTH=160;
-const NUM_OF_WORKING_DAYS=20;
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const WAGE_PER_HOUR=20;
-let totalEmpHrs=0;
-let totalWorkingDays=0;
-let empDailyWageArr=new Array();
- 
-function calculateDailyWage(empHrs)
-{
-  return empHrs*WAGE_PER_HOUR;
+//UC1 check employee is present or absent
+const IS_ABSENT = 0
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if(empCheck == IS_ABSENT){
+    console.log("Employee is Absent");
+    return;
 }
-
-function getWorkingHours(empCheck)
-{
-    switch(empCheck)
-    {
-       case IS_PART_TIME:
-            return 4;
-       case IS_FULL_TIME:
-            return 8;
-       default:
-            return 0;
-   }
+else{
+    console.log("Employee is Present");
 }
-
-while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-{
-  totalWorkingDays++;
-  let empCheck=Math.floor(Math.random() * 10) % 3;
-  let empHrs=getWorkingHours(empCheck);
-  totalEmpHrs+=empHrs;
-  empDailyWageArr.push(calculateDailyWage(empHrs));
-}
- 
-let empWage=calculateDailyWage(totalEmpHrs);
-console.log("Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
- 
-
