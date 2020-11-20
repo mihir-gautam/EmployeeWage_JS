f8e3153b97b9cd7abb19824f12f0e30aaaab9a91
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-21 00:40:49 +0530

    [Mihir] Add. regex validation for name added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -117,7 +117,12 @@ class EmployeePayrollData{
           this.startDate = params[4];
      }
      get name() { return this._name;}
-     set name(name) { this._name = name; }
+     set name(name) { 
+          let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+          if(nameRegex.test(name))
+            this._name = name; 
+          else throw 'Name is Incorrect!';
+     }
      
      toString() {
           const options = {year: 'numeric',month: 'long',day: 'numeric'};
@@ -129,8 +134,12 @@ class EmployeePayrollData{
      }
      let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000); 
      console.log(employeePayrollData.toString()); 
-     employeePayrollData.name = "john";
-     console.log(employeePayrollData.toString());
+     try{
+          employeePayrollData.name = "John";
+          console.log(employeePayrollData.toString());
+     } catch(e){
+          console.error(e);
+     }
      let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F", new Date());
      console.log(newEmployeePayrollData.toString());
      
