0b1fe09699aaa5ab8fce15ea4db989e4ab8ecf3c
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-21 00:07:03 +0530

    [Mihir] Add. Employee payroll data class created

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -101,3 +101,26 @@ let nonWorkingDayNums = empDailyHrsAndWageArr
                          .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
                          .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
 console.log("UC 11D NonWorkingDayNums: "+nonWorkingDayNums);
+
+//UC 11 Create a new Employee payroll class
+class EmployeePayrollData{
+     id;
+     salary;
+
+     constructor(id, name, salary) { 
+          this.id = id;
+          this.name = name;
+          this.salary = salary;
+     }
+     get name() { return this._name;}
+     set name(name) { this._name = name; }
+     
+     toString() {
+          return "id=" + this.id + ", name='" + this.name + ", salary=" + this.salary;
+          }
+     }
+     let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000); 
+     console.log(employeePayrollData.toString()); 
+     employeePayrollData.name = "john";
+     console.log(employeePayrollData.toString());
+     
